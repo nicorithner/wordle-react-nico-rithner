0d8ac7cc251a98e368b0378be03b85197d04fb47
Nico Rithner <nicorithner@gmail.com>
2025-03-18 09:49:43 -0600

    Initial Commit. Add Wordle logo and icon
    Logo create with canva

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import logo from "../public/wordle-logo.png";
+export default function App() {
+  return (
+    <>
+      <img src={logo} alt="Wordle logo" />
+      <h1>Hello World!</h1>
+    </>
+  );
+}
