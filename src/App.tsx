f37a7eef8388cc585e600e4c4a0f87d7d367f61f
Nico Rithner <nicorithner@gmail.com>
2025-03-19 19:27:22 -0600

    Move images to src/assets/images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import logo from "../public/wordle-logo.png";
+import logo from "../src/assets/images/wordle-logo-transparent-bkg.png";
 export default function App() {
   return (
     <>
       <img src={logo} alt="Wordle logo" />
-      <h1>Hello World!</h1>
+      <h1>Hello Wordle!</h1>
     </>
   );
 }
