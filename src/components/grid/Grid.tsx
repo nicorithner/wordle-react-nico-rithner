e897366ce8c981bf419db2b88a55a2cd5ce1e7c5
Nico Rithner <nicorithner@gmail.com>
2025-03-20 09:47:25 -0600

    Implement initial empty grid

---
+++ src/components/grid/Grid.tsx
@@ -0,0 +1,22 @@
+import "./Grid.css";
+export default function Grid() {
+  // Calculate empty rows
+  const emptyRows = Array.from({ length: 5 }, () => "");
+
+  return (
+    <div className="game-container">
+      <div className="grid">
+        {emptyRows.map((_, rowIndex) => (
+          <div key={`empty-${rowIndex}`} className="row">
+            {Array.from({ length: 5 }, (_, colIndex) => (
+              <div
+                key={`empty-cell-${rowIndex}-${colIndex}`}
+                className="cell"
+              ></div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
