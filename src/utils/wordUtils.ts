48ac33cf170b6fc93e70c708b10cf9abd68ab54c
Nico Rithner <nicorithner@gmail.com>
2025-03-19 22:00:06 -0600

    Create evaluateGuess function

---
+++ src/utils/wordUtils.ts
@@ -0,0 +1,26 @@
+import { LetterState } from "../types/types";
+
+export const evaluateGuess = (guess: string, target: string): LetterState[] => {
+  // Temporary console.log for manual testing
+  console.log("Guess: ", guess, " - ", "Target: ", target);
+  const result: LetterState[] = Array(5).fill("absent");
+  const targetLetters: (string | null)[] = [...target];
+
+  for (let i = 0; i < 5; i++) {
+    if (guess[i] === target[i]) {
+      result[i] = "correct";
+      targetLetters[i] = null;
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    if (result[i] !== "correct") {
+      const index = targetLetters.indexOf(guess[i]);
+      if (index > -1) {
+        result[i] = "present";
+        targetLetters[index] = null;
+      }
+    }
+  }
+  return result;
+};
