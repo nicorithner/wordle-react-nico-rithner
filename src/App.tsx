47eb1b36398a110d221ba3d85150295cfbeebf97
Nico Rithner <nicorithner@gmail.com>
2025-03-20 10:21:40 -0600

    Add keyboard. Move container to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import logo from "../src/assets/images/wordle-logo-transparent-bkg.png";
 import Grid from "./components/grid/Grid";
+import Keyboard from "./components/keyboard/Keyboard";
 export default function App() {
   return (
     <>
       <img src={logo} alt="Wordle logo" />
       <h1>Hello Wordle!</h1>
-    <Grid/>
+      <div className="game-container">
+        <Grid />
+        <Keyboard />
+      </div>
     </>
   );
 }
