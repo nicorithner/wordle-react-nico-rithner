dca9405a18a4ec8e8949262a734de5417a6e2145
Nico Rithner <nicorithner@gmail.com>
2025-03-25 09:11:10 -0600

    Add useEffect missing dependency

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ export default function App() {
     setGameWon(false);
     setUsedKeys({});
     setShowWelcome(false);
-  }, [shuffledWords]);
+  }, [shuffledWords, wordIndex]);
 
   const backToWelcome = useCallback(() => {
     setShowWelcome(true);
